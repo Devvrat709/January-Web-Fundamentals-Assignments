98d456ee47c8dc8118e2bfb4696dd73f147ac80c
Dev Vrat <devvrat709@gmail.com>
2022-03-01 22:58:47 +0530

    commit

---
+++ March/week 1/Assignment 2/script.js
@@ -0,0 +1,36 @@
+function clearErrors(){
+
+    errors = document.getElementsByClassName('formerror');
+    for(let item of errors)
+    {
+        item.innerHTML = "";
+    }
+
+
+}
+function seterror(id, error){
+    element = document.getElementById(id);
+    element.getElementsByClassName('formerror')[0].innerHTML = error;
+
+}
+
+function validateForm(){
+    var returnval = true;
+    clearErrors();
+
+    var password = document.forms['myForm']["fpass"].value;
+    if (password.length < 6){
+
+        seterror("pass", "*Password should be atleast 6 characters long!");
+        returnval = false;
+    }
+
+    var cpassword = document.forms['myForm']["fcpass"].value;
+    if (cpassword != password){
+        seterror("cpass", "*Password and Confirm password should match!");
+        returnval = false;
+    }
+
+    return returnval;
+}
+
