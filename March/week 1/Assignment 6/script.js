2daf9b3714d9f026f293b8ae425e847265a78c69
Dev Vrat <devvrat709@gmail.com>
2022-03-06 00:16:30 +0530

    commit

---
+++ March/week 1/Assignment 6/script.js
@@ -0,0 +1,12 @@
+var num1 = 10
+var num2 = 20
+
+console.log("num1 before swap",num1)
+console.log("num2 before swap",num2)
+
+num1 = num1 + num2
+num2 = num1 - num2
+num1 = num1 - num2
+
+console.log("num1 after swap",num1)
+console.log("num2 after swap",num2)
